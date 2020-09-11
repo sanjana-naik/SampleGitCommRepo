24a9e4c3fbe1d89563561b710cf7d2b600dc9967
sanjana-naik <sanjananaik694@gmail.com>
2020-09-11 12:05:50 +0530

    First commit GitSample added

---
+++ src/com/gitcomm/pac/GitSample.java
@@ -0,0 +1,13 @@
+package com.gitcomm.pac;
+
+public class GitSample {
+
+	public static void main(String[] args) {
+		String s="Sanjana";
+		String rev="";
+		for (int i = s.length(); i >=0; i--) {
+			
+			
+		}
+	}
+}
